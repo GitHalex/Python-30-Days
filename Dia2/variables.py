6342867edd90d7cebbc20a50ce13e5c959217ca3
GitHalex <calex1731@gmail.com>
2022-12-01 09:50:15 -0400

    Dia 2 completado

---
+++ Dia2/variables.py
@@ -0,0 +1,90 @@
+# Dia 2: 30 dias de programacion
+name = 'Alex'
+print(f'{name} - tipo {type(name)}')
+
+last_name = 'Calcina'
+print(f'{last_name} - tipo {type(last_name)}')
+
+full_name = 'Hanyinson Alex Calcina Bernal'
+print(f'{full_name} - tipo {type(full_name)}')
+
+country = 'Bolivia'
+print(f'{country} - {type(country)}')
+
+city = 'Uyuni'
+print(f'{city} - {type(city)}') 
+
+age = 28
+print(f'{age} - {type(age)}') 
+
+year = 2022
+print(f'{year} - {type(year)}') 
+
+is_married = False
+print(f'{is_married} - {type(is_married)}') 
+
+is_true = False
+print(f'{is_true} - {type(is_true)}') 
+
+is_light_on = True
+print(f'{is_light_on} - {type(is_light_on)}') 
+
+nombre, universidad, ciudad = 'Hanyinson', 'San Simon', 'Cochabamba'
+print('###################    Parte 2 #######################')
+print(len(name))
+print(len(name) == len(last_name))
+num_one = 5
+num_two = 4
+total = num_one+num_two
+diff = num_one-num_two
+producto = num_two*num_one
+div = num_two/num_one
+mod = num_two%num_one
+exp = num_one**num_two
+floor_division = num_one//num_two
+
+radio = int(input('Ingrese en Radio: '))
+PI = 3.1416
+area_of_circle = PI*radio**2
+print(area_of_circle)
+circum_of_circle = 2*PI*radio
+print(circum_of_circle)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
