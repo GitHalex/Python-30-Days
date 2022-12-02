eee3b80e2aa2d8bed8333da0ccb12deaafb5f904
GitHalex <calex1731@gmail.com>
2022-12-01 22:35:42 -0400

    ejercicios del dia 3 terminado

---
+++ Dia3/operators.py
@@ -0,0 +1,27 @@
+""" age = 28
+altura = 1.6
+num_complex = 3+3j """
+
+""" base = int(input('ingrese la base: '))
+altura = int(input('ingrese la altura: '))
+area = (base*altura)/2
+print(f'the area od the triangle is {int(area)}') """
+
+""" a = int(input('side a: '))
+b = int(input('side b: '))
+c = int(input('side c: '))
+print(f'Perimter of the triangle is {a+b+c} ') """
+
+""" alto = int(input('alto: '))
+ancho = int(input('ancho: '))
+area = alto*ancho
+print(f'Area: {area=}') 
+print(f'Perimetro: {2*(largo+ancho)}')"""
+
+PI = 3.14
+radio = int(input('ingrese el radio del circulo: '))
+
+
+
+
+
